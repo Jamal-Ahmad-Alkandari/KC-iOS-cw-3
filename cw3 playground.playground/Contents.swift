0b502d1d58135621a90f9c7bbd20c94d13d5221c
Jamal-Ahmad-Alkandari <66321501+Jamal-Ahmad-Alkandari@users.noreply.github.com>
2022-08-03 20:01:44 +0300

    done 😎

---
+++ cw3 playground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var name = "Jamal"
+var favouriteNumber : Int = 1
+var hobby: String = "Cooking"
+var BirthDay : String = "08 / 11 / 2004"
+
+
+print("Hello, my name is \(name), my favourit number is \(favouriteNumber) and I like \(hobby). \n I was born in \(BirthDay)")
+
---
+++ cw3/cw3/ContentView.swift
@@ -0,0 +1,102 @@
+//
+//  ContentView.swift
+//  cw3
+//
+//  Created by Jamal Al-kandari on 03/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack{ // Zstack -->
+            
+            
+            Image("DarkWeb")
+            
+            
+            VStack{ //Vstack (1) -->
+                
+                Text("Welcome back : \t\(firstname) \(secondtname) \n\(thirdtname) ").foregroundColor(Color.white)
+                    .font(.system(size: 25))
+                
+                Text("Welcome back : \t\(firstname) \(secondtname) \n\(thirdtname) ").foregroundColor(Color.white)
+                    .font(.system(size: 25))
+                
+                
+               
+                
+                TextField("\tEnter your username", text:$username)
+                    .background(RoundedRectangle(cornerRadius: 6))
+                    .foregroundColor(Color.white.opacity(0.6))
+                    .padding()
+                    .frame(width: 350, height: 50, alignment: .top)
+                
+               
+                
+                VStack{ // Vstack (2) -->
+                    
+                    
+                    TextField("\tFirst name", text:$firstname)
+                        .background(RoundedRectangle(cornerRadius: 6))
+                        .foregroundColor(Color.white.opacity(0.6))
+                        .padding()
+                        .clipShape(RoundedRectangle(cornerRadius: 100))
+                        .frame(width: 350, height: 50, alignment: .top)
+                    
+                    
+                  
+                     
+                     TextField("\tSecond name", text:$secondtname)
+                        .background(RoundedRectangle(cornerRadius: 6))
+                        .foregroundColor(Color.white .opacity(0.6))
+                         .padding()
+                         .clipShape(RoundedRectangle(cornerRadius: 100))
+                         .frame(width: 350, height: 50, alignment: .top)
+                     
+                     
+                     TextField("\tThird name", text:$thirdtname)
+                        .background(RoundedRectangle(cornerRadius: 6))
+                        .foregroundColor(Color.white .opacity(0.6))
+                         .padding()
+                         .clipShape(RoundedRectangle(cornerRadius: 100))
+                         .frame(width: 350, height: 50, alignment: .top)
+                    
+                 
+                     
+                    
+                    
+                } // Vstack (2) <--
+                
+            
+                
+              
+                
+                
+            } // Vstack (1) <--
+            
+            
+            
+        } // Zstack <--
+        
+        
+       
+       
+        
+        
+    }
+    
+    @State var username = ""
+    @State var firstname = ""
+    @State var secondtname = ""
+    @State var thirdtname = ""
+
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
